0308aa5a67af6be03e41952e5f6a91da4bc5d964
Uladzislau Chabukin <vladchebukin@yandex.ru>
2024-10-18 18:53:32 +0300

    add task_1

---
+++ homework/uladzislau_chabukin/Homework_3/task_1.py
@@ -0,0 +1,10 @@
+a = 24
+b = 4
+
+sum_of_a_and_b = a + b
+difference_of_a_and_b = a - b
+product_of_a_and_b = a * b
+
+print(
+    f"Сумма = {sum_of_a_and_b}", f"Разность = {difference_of_a_and_b}", f"Произведение = {product_of_a_and_b}", sep='\n'
+)
