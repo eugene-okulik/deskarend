03fbbb7fb650fa2068bd3cc122188023fcccdf34
Uladzislau Chabukin <vladchebukin@yandex.ru>
2024-10-20 14:57:26 +0300

    fix key in value 'dict'

--- homework/uladzislau_chabukin/Homework_4/task.py
+++ homework/uladzislau_chabukin/Homework_4/task.py
@@ -10,7 +10,7 @@ print(my_dict['tuple'][-1])
 my_dict['list'].append(6.0)
 my_dict['list'].pop(1)
 
-my_dict['dict']['i am a tuple'] = 'not really'
+my_dict['dict'][('i am a tuple',)] = 'not really'
 my_dict['dict'].pop('three')
 
 my_dict['set'].add('random_6')
