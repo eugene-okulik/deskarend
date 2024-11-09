2494442ad0c164f290c578129337fa4ea30bd409
Uladzislau Chabukin <vladchebukin@yandex.ru>
2024-11-09 17:23:17 +0300

    add allure

---
+++ homework/uladzislau_chabukin/Homework_20/data.py
@@ -0,0 +1,19 @@
+class Urls:
+    BASE_URL = 'http://167.172.172.115:52353'
+
+
+class Objects:
+    obj_1 = {
+        "name": "First object",
+        "data": {"color": "white", "size": "big"}
+    }
+
+    obj_2 = {
+        "name": "Second object",
+        "data": {"color": "green", "size": "small"}
+    }
+
+    obj_3 = {
+        "name": "Third object",
+        "data": {"color": "red", "size": "Biggest"}
+    }
